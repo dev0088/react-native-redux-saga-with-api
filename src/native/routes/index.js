1690e02bc7a0056450636ed8ad08c221188a60fc
ninjadev999 <mikenarkss@gmail.com>
2018-06-03 20:04:42 -0700

    created react-native app with redux-saga middleware and RESTful API mode

---
+++ src/native/routes/index.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import { Scene, Tabs, Stack } from 'react-native-router-flux';
+import { Icon } from 'native-base';
+
+import DefaultProps from '../constants/navigation';
+import AppConfig from '../../constants/config';
+
+import RecipesContainer from '../../containers/Recipes';
+import RecipesComponent from '../components/Recipes';
+import RecipeViewComponent from '../components/Recipe';
+
+import SignUpContainer from '../../containers/SignUp';
+import SignUpComponent from '../components/SignUp';
+
+import LoginContainer from '../../containers/Login';
+import LoginComponent from '../components/Login';
+
+import ForgotPasswordContainer from '../../containers/ForgotPassword';
+import ForgotPasswordComponent from '../components/ForgotPassword';
+
+import LocaleContainer from '../../containers/Locale';
+import LocaleComponent from '../components/Locale';
+
+import UpdateProfileContainer from '../../containers/UpdateProfile';
+import UpdateProfileComponent from '../components/UpdateProfile';
+
+import MemberContainer from '../../containers/Member';
+import ProfileComponent from '../components/Profile';
+
+import AboutComponent from '../components/About';
+
+const Index = (
+  <Stack>
+
+    <Scene hideNavBar>
+
+        <Stack
+          key="recipes"
+          title="RECIPES"
+          icon={() => <Icon name="book" {...DefaultProps.icons} />}
+          {...DefaultProps.navbarProps}
+        >
+          <Scene key="recipes" component={RecipesContainer} Layout={RecipesComponent} />
+        </Stack>
+        <Stack initial>
+          <Scene
+            key="login"
+            title="LOGIN"
+            {...DefaultProps.navbarProps}
+            component={LoginContainer}
+            Layout={LoginComponent}
+          />
+          <Scene
+            back
+            key="signUp"
+            title="SIGN UP"
+            {...DefaultProps.navbarProps}
+            component={SignUpContainer}
+            Layout={SignUpComponent}
+          />
+          <Scene
+            back
+            key="forgotPassword"
+            title="FORGOT PASSWORD"
+            {...DefaultProps.navbarProps}
+            component={ForgotPasswordContainer}
+            Layout={ForgotPasswordComponent}
+          />
+        </Stack>
+        <Stack
+          key="profile"
+          title="PROFILE"
+          icon={() => <Icon name="contact" {...DefaultProps.icons} />}
+          {...DefaultProps.navbarProps}
+        >
+          <Scene key="profileHome" component={MemberContainer} Layout={ProfileComponent} />
+          <Scene
+            back
+            key="login"
+            title="LOGIN"
+            {...DefaultProps.navbarProps}
+            component={LoginContainer}
+            Layout={LoginComponent}
+          />
+          <Scene
+            back
+            key="home"
+            title={AppConfig.appName.toUpperCase()}
+            {...DefaultProps.navbarProps}
+            component={AboutComponent} 
+            Layout={LoginComponent}
+          />
+       
+          <Scene
+            back
+            key="signUp"
+            title="SIGN UP"
+            {...DefaultProps.navbarProps}
+            component={SignUpContainer}
+            Layout={SignUpComponent}
+          />
+          <Scene
+            back
+            key="forgotPassword"
+            title="FORGOT PASSWORD"
+            {...DefaultProps.navbarProps}
+            component={ForgotPasswordContainer}
+            Layout={ForgotPasswordComponent}
+          />
+          <Scene
+            back
+            key="locale"
+            title="CHANGE LANGUAGE"
+            {...DefaultProps.navbarProps}
+            component={LocaleContainer}
+            Layout={LocaleComponent}
+          />
+          <Scene
+            back
+            key="updateProfile"
+            title="UPDATE PROFILE"
+            {...DefaultProps.navbarProps}
+            component={UpdateProfileContainer}
+            Layout={UpdateProfileComponent}
+          />
+        </Stack>
+    </Scene>
+
+    <Scene
+      back
+      clone
+      key="recipe"
+      title="RECIPE"
+      {...DefaultProps.navbarProps}
+      component={RecipesContainer}
+      Layout={RecipeViewComponent}
+    />
+  </Stack>
+);
+
+export default Index;
